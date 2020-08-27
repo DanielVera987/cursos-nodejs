a2207137982ec62ab8d865fc96ce47dee89104ec
danielvera987 <danielveraangulo703@gmail.com>
2020-08-26 19:39:57 -0500

    Subida de archivos

--- 02ModulosNMP/01SubirArchivo/upload.js
+++ 02ModulosNMP/01SubirArchivo/upload.js
@@ -17,6 +17,37 @@ function serverUpload(req, res)
       </form>`
       );
   }
+
+  if(req.method == 'POST' && req.url == '/upload')
+  { 
+    let form = new formidable.IncomingForm()
+
+    form 
+        .parse(req, (err, fields, files) => {
+          res.writeHead(200, {'Content-Type': 'text/html'})
+          res.write('<h1>Archivos Recibidos</h1>' + util.inspect({files: files}))
+          res.end()
+        })
+        .on('progress', (bytesReceived, bytesExpected) => {
+          let percentCompleted = (bytesReceived / bytesExpected) * 100
+          console.log(percentCompleted)
+        })
+        .on('error', (err) => {
+          console.log(err)
+        })
+        .on('end', function(fields, files){
+          
+          let tempPath = this.openedFiles[0].path, //Ubicación temporal del archivo que se sube
+              fileName = this.openedFiles[0].name, //El nombre del archivo subido
+              newLocation = `./upload/${fileName}` //Nueva Ubicacion
+
+              fse.copy(tempPath, newLocation, function(err){
+                return (err) ? console.log(err) : console.log('El archivo se subio con exito')
+              })          
+        })
+
+    return
+  }
 }
 
 http.listen(3000)

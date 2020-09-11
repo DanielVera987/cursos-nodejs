18d509d50edc264810c06a4bb5d1badfe1d48bdf
danielvera987 <danielveraangulo703@gmail.com>
2020-09-10 20:33:31 -0500

    API CRUD con Mysql 5/5

--- 07ApiCRUDMySQL/routes/index.js
+++ 07ApiCRUDMySQL/routes/index.js
@@ -77,7 +77,36 @@ router
         })
       })
     })
-    .use( error404 )
+    .post('/actualizar/:movie_id', (req, res, next) => {
+      req.getConnection((err, movies) => {
 
+        let movie = {
+          movie_id: req.body.movie_id,
+          title: req.body.title,
+          release_year: req.body.release_year,
+          rating: req.body.rating,
+          image: req.body.image
+        }
+
+        console.log(movie)
+
+        movies.query('UPDATE movie SET ? WHERE movie_id = ?', [movie, movie.movie_id], (err, rows) => {
+          return (err) ? res.redirect('/editar/:movie_id') : res.redirect('/') 
+        })
+      })
+    })
+    .post('/eliminar/:movie_id', (req, res, next) => {
+      let movie_id = req.params.movie_id
+      console.log(movie_id)
+
+      req.getConnection((err, movies) => {
+        movies.query('DELETE FROM movie WHERE movie_id=?', movie_id, (err, rows) => {
+          console.log(err, '---', rows)
+          return (err) ? next(new Error('Registro no encontrado')) : res.redirect('/')
+            
+        })
+      })
+    })
+    .use( error404 )
 
 module.exports = router
